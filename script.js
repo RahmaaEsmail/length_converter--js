00a4509ab8521d36cb816ea5dbd508e997151543
RahmaaEsmail <rahmaaesmail26@gmail.com>
2023-08-05 10:24:55 +0300

    Create the inputs in JavaScript

--- script.js
+++ script.js
@@ -1,30 +1,71 @@
-const resetBtn = document.querySelector(".reset-btn");
-
-const getData = () => {
-    const inputs = document.querySelectorAll("input");
+const getData = (container) => {
+    const inputs = container.querySelectorAll("input");
 
     inputs.forEach(input => {
         input.onkeyup = function() {
-           let  meter = this.id == "kelometer" ? parseFloat(this.value * 1000) : this.id == "centi" ? parseFloat(this.value / 100) : this.id == "inch" ? parseFloat(this.value / 39.37) : this.id == "feet" ? parseFloat(this.value / 3.281) : this.id == "yards" ? parseFloat(this.value / 1.094) : this.id == "mile" ? parseFloat(this.value * 1609 ): parseFloat(this.value)
-           this.value = parseFloat(this.value)
-           displayData(meter)
+           let meter = this.id == "kelometer" ? parseFloat(this.value * 1000) : this.id == "centi" ? parseFloat(this.value / 100) : this.id == "inch" ? parseFloat(this.value / 39.37) : this.id == "feet" ? parseFloat(this.value / 3.281) : this.id == "yards" ? parseFloat(this.value / 1.094) : this.id == "mile" ? parseFloat(this.value * 1609 ): parseFloat(this.value)
+           this.value = parseFloat(this.value) || ''
+           displayData(meter , inputs)
         }
-
     })
 }
-getData()
+
+
+const createElements = () => {
+    const container = document.querySelector(".container");
+    let html = `
+    <div>
+            <label for="meter">Enter length in Meters here</label>
+            <input type="number" placeholder="Enter length in Meters here" id="meter">
+        </div>
+
+        <div>
+            <label for="kelometer">Enter length in Kilometers here</label>
+            <input type="number" placeholder="Enter length in Kilometers here" id="kelometer">
+        </div>
+
+        <div>
+            <label for="centi">Enter length in Centimeters here</label>
+            <input type="number" placeholder="Enter length in Centimeters here" id="centi">
+        </div>
+
+        <div>
+            <label for="inch">Enter length in Inches here</label>
+            <input type="number" placeholder="Enter length in Inches here" id="inch">
+        </div>
+
+        <div>
+            <label for="feet">Enter length in Feets here</label>
+            <input type="number" placeholder="Enter length in Feets here" id="feet">
+        </div>
+
+        <div>
+            <label for="yards">Enter length in Yards here</label>
+            <input type="number" placeholder="Enter length in Yards here" id="yards">
+        </div>
+
+        <div>
+            <label for="mile">Enter length in Miles here</label>
+            <input type="number" placeholder="Enter length in Miles here" id="mile">
+        </div>
+
+        <div class="reset-btn">
+            <button>Reset</button>
+        </div>
+    `
+
+    container.innerHTML += html
+    return container
+}
 
 
 
+const displayData = (meter , inputs) => {
+    let array = [meter, meter / 1000, meter * 100, meter * 39.37, meter * 3.281, meter * 1.094, meter / 1609]
+    for(let i =0;i<inputs.length ;i++) {
+        inputs[i].value = array[i];
+    }
 
-const displayData = (meter) => {
-    document.getElementById("meter").value = meter
-    document.getElementById("kelometer").value = meter / 1000
-    document.getElementById("centi").value = meter * 100
-    document.getElementById("inch").value = meter * 39.37
-    document.getElementById("feet").value = meter * 3.281
-    document.getElementById("yards").value = meter * 1.094
-    document.getElementById("mile").value = meter / 1609
 }
 
 const resetInputs = () => {
@@ -34,4 +75,11 @@ const resetInputs = () => {
   })
 }
 
-resetBtn.addEventListener("click",resetInputs)
+
+const init = ()=>{
+   const dataContainer = createElements()
+   getData(dataContainer)
+   const resetBtn = dataContainer.querySelector(".reset-btn");
+   resetBtn.addEventListener("click",resetInputs)
+}
+init()
